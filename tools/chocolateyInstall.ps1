452df9eaeed3f48027e3016e2a14181e13e82e01
zersh01 <zersh01@gmail.com>
2020-06-05 20:39:34 +0300

    add source

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,49 @@
+﻿$packageName = 'k-litecodecpackfull'
+$installerType = 'exe'
+$url = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1550_Standard.exe'
+$silentArgs = '/VERYSILENT /NORESTART'
+                                         
+$checksum = '9b0c25cfc376e9e5f397d9852c5465cf'
+
+$checksumType = 'md5'
+ 
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -Checksum "$checksum" -ChecksumType "$checksumType"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
