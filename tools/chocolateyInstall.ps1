6ce5e0d432341f5e8fb839695000a9e26f20a12e
zersh01 <zersh01@gmail.com>
2021-02-21 16:13:24 +0300

    rel. 16.0.2

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,13 +1,14 @@
 ﻿$packageName = 'k-litecodecpack-standard'
 $installerType = 'exe'
-$url = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1590_Standard.exe'
+$url = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1605_Standard.exe'
 $silentArgs = '/VERYSILENT /NORESTART'
                                          
-$checksum = 'eccd2e14bb275d8f5c050f54ff1c4534'
+$checksum = '1f9a9fc506c96abfabe51e63183bcc93'
 
 $checksumType = 'md5'
  
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -Checksum "$checksum" -ChecksumType "$checksumType"
+
 
 
 
--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -60,3 +60,4 @@ if ($uninstallString -ne "") {
 
 
 
+
