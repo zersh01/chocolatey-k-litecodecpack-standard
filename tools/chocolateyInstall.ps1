20727a6f1f7b250f773c1f222307336ffc3eb397
zersh <zersh@mail.ru>
2026-02-22 19:18:00 +0300

    tmp_19.5.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = 'k-litecodecpack-standard'
 $installerType = 'exe'
-$url = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1945_Standard.exe'
+$url = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1950_Standard.exe'
 $silentArgs = '/VERYSILENT /NORESTART'
                                          
-$checksum = '118affbc123f97eeeab169b5786c5b59'
+$checksum = '962ffffd936f5aee95b51d2c443d9dad'
 
 $checksumType = 'md5'
  
